7582c12ec319ea3696b35f5ad717f41dcc219fb1
Beimar Medina - Contractor <Beimar.Medina@scdhhs.gov>
2020-01-06 20:56:00 -0500

    comments

---
+++ PowerApps/Apps/DHHS-REBUTTAL/Installation/Lists/create_error_codes_list.ps1
@@ -0,0 +1,344 @@
+#Import the required DLL
+Import-Module 'C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll'
+#OR
+#Add-Type -Path 'C:\Program Files\Common Files\Microsoft Shared\Web Server Extensions\16\ISAPI\Microsoft.SharePoint.Client.dll'
+
+#Mysite URL
+$site = 'https://schhs.sharepoint.com/sites/HREPSMRebuttal'
+
+#Admin User Principal Name
+$admin = 'beimar.medina@scdhhs.gov'
+
+#Get Password as secure String
+$password = Read-Host 'Enter Password' -AsSecureString
+#$password = ConvertTo-SecureString "YourPassword" -asplaintext -force
+#Get the Client Context and Bind the Site Collection
+$context = New-Object Microsoft.SharePoint.Client.ClientContext($site)
+
+#Authenticate
+$credentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($admin , $password)
+$context.Credentials = $credentials
+
+#Create List
+$listCreationInformation = New-Object Microsoft.SharePoint.Client.ListCreationInformation
+$listCreationInformation.Title = "ErrorCodes"
+$listCreationInformation.Description = "ErrorCodes created through PowerShell"
+$listCreationInformation.TemplateType = 100
+$list = $context.Web.Lists.Add($listCreationInformation)
+$context.Load($list)
+$context.ExecuteQuery()
+
+#Hide the list
+#$list.Hidden = $True
+#$list.Update()
+#$context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "1.1 - Application was not signed"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "1.2 - Application could not be located in case file"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "2.1 - Case record is missing social security or proof of application"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "3.1 - Case record is missing citizenship/ lawful presence documentation"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "3.2 - Worker failed to accept appropriate citizenship/lawful presence documentation"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "3.3 - Worker failed to give reasonable opportunity to verify citizenship/lawful presence"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "3.4 - Worker failed to terminate eligibility when citizenship/lawful presence was not verified within the given time period"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.1 - Client was not aged or disabled"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.2 - Client did not meet living arrangements or  waiver enrollment/slot approval requirements"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.4 - Client was not in Foster Care"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.5 - Client was not pregnant"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.6 - No minor child in home/child aged out of PHC"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.7 - Client moved to TMA for reasons other than MPPM 205.01"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.8 - Infant did not meet requirements for deemed baby MPPM 204.02.02"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.9 - Client did not meet requirements for Working Disabled (MPPM 307) or Pass-Alongs (MPPM 404)"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "4.1 - Client was categorically eligible but was not given coverage"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "5.1 - Individual keyed as applying but was not applying or visa versa"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "5.2 - Incorrect household/BG  group-Member included but should have been excluded or visa versa"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "5.3 - Tax filer status unknown-follow up was needed"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "5.4 - Tax filer status incorrect in workbook and/or Cúram"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "5.5 - Tax dependent status keyed incorrectly"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "5.6 - Relationship incorrect in workbook and/or system of record"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "6.1 - Resources were not verified correctly"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "6.2 - Income was not verified correctly"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "6.3 - Income/resource verification available in case record or data match"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "7.1 - Income was not keyed correctly in system of record"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "7.2 - Resources were not keyed correctly in system of record"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.1 - Budgeting error-income was included that should not have been"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.2 - Budgeting error-income was excluded that should not have been"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.3 - Income was calculated incorrectly"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.4 - Income updated into system was incorrect"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.5 - Self-employment was budgeted incorrectly"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.6 - Verification of cafeteria plan participation in case record. The reduced gross amount of income was not budgeted"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.7 - Resources were included that should not have been"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.8 - Resources were excluded that should have been included"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.9 - Resources were calculated incorrectly"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "8.1 - Resources updated into system incorrectly"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "9.1 - Client alleged TPL.  Worker failed to follow up to obtain evidence"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "9.2 - TPL indicator is incorrect in MEDS/Cúram"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "9.3 - 3230 was not completed by worker"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "10.1 - An accurate look-back was not completed"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "11.1 - Penalty period for Transfer of Assets was computed incorrectly"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "11.2 - Penalty period did not begin on correct start date"
+$item.Update()
+$Context.ExecuteQuery()
+
+#Adds an item to the list
+$listItemInfo = New-Object Microsoft.SharePoint.Client.ListItemCreationInformation
+$item = $list.AddItem($listItemInfo)
+$item["Title"] = "12.1 - The Level of Care could not be located in case record or Phoenix"
+$item.Update()
+$Context.ExecuteQuery()
+
+Write-Output "List has been created successfully!."
---
+++ SharePoint/Online/SPfx/nio/custom/SiteAssets/nio/js/nio.js
@@ -0,0 +1,13 @@
+'use strict';
+
+jQuery(document).ready(function () {
+
+    // Check for FileReader API (HTML5) support.
+    if (!window.FileReader) {
+        alert('This browser does not support the FileReader API.');
+    }        
+
+    console.log('Up and Running .....  !!!!!!');
+    
+                                      
+});
